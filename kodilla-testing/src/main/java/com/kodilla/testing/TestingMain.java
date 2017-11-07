d28bcc7980e59a3566741bb2119d8bf54c663020
arkadyj <szelaga@gmail.com>
2017-11-07 20:57:54 +0100

    do_zad_6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,1 @@
-package com.kodilla.testing;
-
-public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
-    }
-}
+package com.kodilla.testing;import com.kodilla.testing.user.SimpleUser;public class TestingMain {    public static void main(String[] args) {        SimpleUser simpleUser = new SimpleUser("TheFormUser");        String result = simpleUser.getUsername();        if (result.equals("TheFormUser")) {            System.out.println("Test OK");        } else {            System.out.println("Error");        }        System.out.println("Test - pierwszy test jednostkowy:");    }}
