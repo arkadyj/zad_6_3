5f2e00d6e84fde26c256e06dd4eca18473b76e4a
karol <88lkarol@gmail.com>
2017-11-09 00:56:38 +0100

    poprawki do 6.3

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -7,9 +7,10 @@ import java.util.List;
 import java.util.Random;
 
 public class CollectionTestSuite {
-
+private   OddNumbersExterminator oddNumbersExterminator;
     @Before
     public void before() {
+        oddNumbersExterminator = new OddNumbersExterminator();
         System.out.println("Beginning test");
     }
 
@@ -21,12 +22,11 @@ public class CollectionTestSuite {
     @Test
     public void testOddNumbersExterminatorEmptyList (){
 
-        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
-
         ArrayList<Integer> lista = new ArrayList<>();
         //oddNumbersExterminator.exterminate(lista);
         System.out.println("Testing empty list");
 
+        //lepiej sprawdzić coś bardziej sensownego, czy nie jest nullem, i czy jest pusta, itp
         Assert.assertTrue(oddNumbersExterminator.exterminate(lista) instanceof List);
 
 
@@ -35,17 +35,26 @@ public class CollectionTestSuite {
     @Test
     public void testOddNumbersExterminatorNormalList () {
 
-        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
-
         ArrayList<Integer> lista = new ArrayList<>();
         Random rand = new Random();
         for (int i=0; i<100; i++) {
+            //rand to nie jest dobry pomysł
             lista.add(rand.nextInt(20)+1);
         }
         System.out.println("Testing filled list");
 
+        //Po co sprawdzasz jakiego jest typu skoro to jest ustawione w definicji metody?
+        //Skoro to test pozytywnego przyapdku to powinieneś sprawdzać czy zostały usunięte odpowiedznie liczby,
+        //tzn stworzyć kolekcje z usuniętymi i porównać z tym co zwróci metoda
         Assert.assertTrue(oddNumbersExterminator.exterminate(lista) instanceof List);
 
 
+    }
+
+    //zaimplementuj pod ten przypadek testowy
+    //i dokończ assercje
+    @Test
+    public void testNull() throws Exception {
+        oddNumbersExterminator.exterminate(null);
     }
 }
